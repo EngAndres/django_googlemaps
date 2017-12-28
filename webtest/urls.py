d4787b1455a3ae2fb9806388660fe0789902d1ae
engandres <jerarquia04@gmail.com>
2017-12-27 21:09:07 -0500

    Djando Views and Urls

--- webtest/urls.py
+++ webtest/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.new_order, name='new_order'),
+    url(r'^$', views.index, name='index'),
+    url(r'^new_order/$', views.new_order, name='new_order'),
+    url(r'^non_delivered/$', views.nondelivered, name='non_delivered'),
+    url(r'^seed_file/$', views.seedfile, name='seed_file'),
+    url(r'^reports_delivered/$', views.reportsdelivered, name='reports_delivered'),	
 ]
--- webtest/views.py
+++ webtest/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'webtest/index.html')
+
 def new_order(request):
     return render(request, 'webtest/new_order.html')
+
+def seedfile(request):
+    return render(request, 'webtest/seed_file.html')
+
+def nondelivered(request):
+    return render(request, 'webtest/non_delivered.html')
+
+def reportsdelivered(request):
+    return render(request, 'webtest/reports_delivered.html')
